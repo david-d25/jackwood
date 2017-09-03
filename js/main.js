d2ce750597502681e837f62e22ed27d27844b27b
David <david-d25@yandex.ru>
2017-09-03 20:01:43 +0300

    Added some sections

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-window.onload = onWindowLoad; // Если кто-то подключит скрипт без defer
+window.onload = onWindowLoad; // Если кто-то подключит скрипт без 'defer'
 
 var solutionNames = [
   "Кухни",
@@ -17,6 +17,7 @@ var solutionNames = [
 
 function onWindowLoad() {
   generateSolutions();
+  initPopups();
 }
 
 function generateSolutions() {
@@ -31,4 +32,31 @@ function generateSolutions() {
           name: solutionNames[index]
         })
       );
+}
+
+function initPopups() {
+  var source = $("#portfolio-item-template").html();
+  var template = Handlebars.compile(source);
+
+  for (var i = 1; i <= 8; i++)
+    $("#portfolio .container")
+      .append(
+        template({
+          index: i
+        })
+      );
+
+  $('#portfolio .container').magnificPopup({
+    delegate: 'div',
+    type: 'image',
+    gallery: {
+      enabled: true
+    },
+    callbacks: {
+      buildControls: function() {
+        this.contentContainer.append(this.arrowLeft.add(this.arrowRight));
+      }
+    },
+    closeMarkup: '<div title="%title%" type="button" class="mfp-close"></button>'
+  });
 }
